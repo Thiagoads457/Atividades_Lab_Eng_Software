99735c6c6a3094ded79327c983be77f98fb4a1a8
Thiagoads457 <thiagoads457@gmail.com>
2024-08-25 14:30:11 -0300

    1430

--- Semana_02/script.js
+++ Semana_02/script.js
@@ -12,3 +12,22 @@ function exibirDataAtual() {
     const dataFormatada = `${diaSemana}, ${dia} de ${mes} de ${ano}`;
     alert(dataFormatada);  
 }
+
+function iniciarRelogio() {
+    const relogioElement = document.getElementById("relogio");
+
+    function atualizarRelogio() {
+        const data = new Date();
+        const horas = String(data.getHours()).padStart(2, '0');
+        const minutos = String(data.getMinutes()).padStart(2, '0');
+        const segundos = String(data.getSeconds()).padStart(2, '0');
+
+        const horarioAtual = `${horas}:${minutos}:${segundos}`;
+        relogioElement.textContent = horarioAtual;
+        
+    }
+
+    atualizarRelogio();  
+    setInterval(atualizarRelogio, 1000);  
+    
+}
